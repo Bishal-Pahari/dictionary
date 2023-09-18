18b775d99406ffde9ff36343e68b895788130e73
Bishal Pahari <bishal.pahari53@gmail.com>
2023-09-18 20:54:15 +0545

    feat: Create initial app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      words: [""],
+      error: [""],
+    };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  componentDidMount() {
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/narco")
+      .then((response) => response.json())
+      .then((wordsCollection) => this.setState({ words: wordsCollection }))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.words.map((word) => (
+          <div key={word.key}>
+            <h1>{word.word}</h1>
+            <p>{word.meaning}</p>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
